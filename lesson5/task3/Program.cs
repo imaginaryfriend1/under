0211fd36f2fee8fc6e8417cc5a34471cc70da892
Julia <kaspiiskya@mail.ru>
2022-10-10 00:35:45 +0300

    38

---
+++ lesson5/task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
+double [] array = {3, 7, 22, 2, 78};
+double count = 0;
+double max = 0;
+double min = array[0];
+for (int i = 0; i < array.Length; i++)
+{   
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+count = max - min;
+Console.WriteLine(string.Join(", ", array));
+Console.WriteLine("Минимальное число "+ min);
+Console.WriteLine("Максимальное число "+ max);
+Console.WriteLine("Разница между числами = " + count);
+
