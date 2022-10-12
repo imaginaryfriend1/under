5d6a05dff9bbf52f0b279ad9d714fc830aad941b
Julia <kaspiiskya@mail.ru>
2022-10-12 23:24:09 +0300

    47

---
+++ lesson6/task1/Program.cs
@@ -0,0 +1,30 @@
+﻿double[,] Fillmassif(int rowsCnt, int columnsCnt)
+{
+    double[,] massif = new double[rowsCnt, columnsCnt];
+    Random rand = new Random();
+    for (int i = 0; i < massif.GetLength(0); i++)
+    {
+        for (int t = 0; t < massif.GetLength(1); t++)
+        {
+            massif[i, t] = rand.NextDouble() * 100 + 20;
+        }
+    }
+    return massif;
+}
+void Printmassif(double[,] massif)
+{
+    for (int i = 0; i < massif.GetLength(0); i++)
+    {
+        for (int t = 0; t < massif.GetLength(1); t++)
+        {
+            Console.Write($"{massif[i, t]:f2}" + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Введите число строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] massif = Fillmassif(m, n);
+Printmassif(massif);
